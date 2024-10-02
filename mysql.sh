a3014b9d7c88022d3a89ac5f23e9fe3eedf36f55
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-02 18:02:25 +0530

    Adding check status to all the scripts.

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,14 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+source common.sh
+
+Print_Task_Heading "Install Mysql server"
+dnf install mysql-server -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Enable and Start mysqld"
+systemctl enable mysqld &>>$LOG
+systemctl start mysqld &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Secure Installation"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG
+Check_Status $?
